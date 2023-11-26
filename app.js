96b00fab20890bb1e6bb619e7021e3c755f374d0
Wratten <daniel.wratten@gmail.com>
2023-11-26 11:04:55 +0800

    randomization now works on each button press, as opposed to only working the once per page refresh

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // have an array containing random strategies
 let stratArray = ["Strat 1", "Strat 2", "Strat 3"];
-let strat = stratArray[Math.floor(Math.random() * stratArray.length)];
 
 // have the randomize button
 const randomizeBtn = document.getElementById("randomizeBtn");
 
 // have a function to use on the button press
 function populateText() {
+  //randomizing strat
+  let strat = stratArray[Math.floor(Math.random() * stratArray.length)];
+  //getting and updating the text field in html with the randomized strat
   document.getElementById("stratDisplayText").textContent = strat;
 }
 
+// call function on button press
 randomizeBtn.addEventListener("click", populateText);
-// read button press / event listener?
-
-// choose one strat at random out of array and populate an empty container with string on button press
